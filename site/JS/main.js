2288bf1b40ca75bdb1f8a0ffdf96df34604e6c9a
itggot-tintin-wihlborg <tintin.wihlborg@elev.ga.ntig.se>
2019-01-17 15:38:13 +0100

    Made a searchbar

--- site/JS/main.js
+++ site/JS/main.js
@@ -1,9 +1,14 @@
 function tagbar(event){
 
-    let tag = document.querySelector(".tag");
     let menu = document.querySelector(".dropdown");
     menu.classList.toggle("show");
 
+
+}
+function searchf(event){
+
+    let search = document.querySelector(".searchbar");
+    search.classList.toggle("showsearch");
 
 
 }
